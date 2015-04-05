548bcceccb086dc34b5ecd19b0f1dc628b1d1c1d
Laurence-Cullen <laurencecullen@live.co.uk>
2015-04-05 12:19:00 +0100

    Rad/degree error fixed

--- src/core_math.c
+++ src/core_math.c
@@ -126,10 +126,9 @@ void colour(struct rgb img_data[256][256], int x, int y, int z, double jdate, do
             rowd = row;
             cold = col;
 
-
-            surface_polar[0] = 0.017453 * (((x + cold/256) / n) * 360.0 - 180.0) + (t_time/DAY_IN_SECONDS)*360;
+            surface_polar[0] = 0.017453 * (((x + cold/256) / n) * 360.0 - 180.0 + (t_time/DAY_IN_SECONDS)*360);
             surface_polar[1] = atan(sinh(M_PI * (1 - 2 * (y + rowd/256) / n)));
-
+            
             polar_to_cart(surface_polar, surface_cart);
 
             anom = anomcalc(surface_cart, Moon, Sun);
